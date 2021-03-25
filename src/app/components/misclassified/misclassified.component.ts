27f2b8986a5201f86b07023c9269a337af343405
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2021-03-25 08:33:31 +0000

    mis class

--- src/app/components/misclassified/misclassified.component.ts
+++ src/app/components/misclassified/misclassified.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-misclassified',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisclassifiedComponent implements OnInit {
 
-  constructor() { }
+  classes : any[] = [];
+  serverURL: string;
+  selected = '1';
+  imgLinks = [];
+
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.serverURL = environment.serverUrl;
+  }
 
   ngOnInit(): void {
+    this.getAllClasses();
+    this.getImgs();
+  }
+  getAllClasses(){
+    this.http.get(this.serverURL + '/allClasses').subscribe((response : any)=>{
+      for(var i in response['classes']){
+        this.classes.push(response['classes'][i])
+      }
+    },error=>{
+      console.log('error')
+    })
+  }
+  async getImgs(){
+    this.imgLinks = [];
+    this.http.get(this.serverURL + `/misclassified?class=${this.selected}`).subscribe((response : any)=>{
+      console.log(response['results'])
+      this.imgLinks = response['results']
+    },error=>{
+      console.log('error')
+    })
   }
 
 }
