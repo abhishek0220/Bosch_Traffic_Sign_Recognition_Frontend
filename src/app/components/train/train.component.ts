aa2792ae1dad13150012905017d1920aea7dc8b9
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2021-03-25 15:57:35 +0000

    train

--- src/app/components/train/train.component.ts
+++ src/app/components/train/train.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-train',
@@ -8,14 +11,42 @@ import { Component, OnInit } from '@angular/core';
 export class TrainComponent implements OnInit {
   ratioValue = 0;
   passKey = "";
-  constructor() { }
+  scolor = "grey";
+  serverURL : string;
+  modelRun = false;
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.serverURL = environment.serverUrl;
+  }
 
   ngOnInit(): void {
+    interval(1000).subscribe(x => {
+      this.getStatus();
+    });
   }
   formatLabel(value: number) {
     return value;
   }
   trainModel(){
-    console.log(this.ratioValue, this.passKey)
+    this.http.get(this.serverURL + `/trainModel?pkey=${this.passKey}&per=${this.ratioValue}`).subscribe((response : any)=>{
+      console.log(response)
+    },error=>{
+      console.log('error')
+    })
+  }
+  getStatus(){
+    this.http.get(this.serverURL + '/modelStatus').subscribe((response : any)=>{
+      if(response['running'] == true){
+        this.scolor = "green accent-3";
+        this.modelRun = true;
+      }
+      else{
+        this.scolor = "grey";
+        this.modelRun = false;
+      }
+    },error=>{
+      console.log('error')
+    })
   }
 }
